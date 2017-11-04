dca2d9db5817cb618521b60cbd1617af620c5a35
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-04 16:00:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function sayHiToGrandma(string){
   if(string='hello!')
     return "I can't hear you!"
   else if(string="I love you, Grandma.")
-    return "YES INDEED"
+    return "I love you, too."
 }
